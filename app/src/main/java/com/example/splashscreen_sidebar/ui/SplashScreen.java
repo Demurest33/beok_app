587696456d6a48d5bad7da88deb2699e51769fab
Javier Jara Renteria <javierjararenteria@gmail.com>
2024-10-27 22:44:09 -0600

    intial commit

---
+++ app/src/main/java/com/example/splashscreen_sidebar/ui/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.splashscreen_sidebar.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.splashscreen_sidebar.MainActivity;
+import com.example.splashscreen_sidebar.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static int tiempoCarga = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, tiempoCarga);
+    }
+}
